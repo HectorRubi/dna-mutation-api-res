08ece205d48a169f583848eddb4ef4e51c9e0671
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-11-15 11:55:05 -0600

    Complete function for /stats path
    Thiss function use methods to get mutations information and return it.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const hasMutation = require('dna-mutation');
 const express = require('express');
 const bodyParser = require('body-parser');
+const Database = require('./utils/database');
 
 const app = express()
 const port = process.env.PORT || 8080
+const db = new Database()
 
 const jsonParser = bodyParser.json()
 
-app.get('/stats', function(req, res) {
-  res.json({ mensaje: '¡Hola Mundo!' })   
+app.get('/stats', async function(req, res) {
+  try {
+    const countMutation = await db.getCountMutations()
+    const countNoMutation = await db.getCountNoMutations()
+    const ratio = countMutation / countNoMutation
+  
+    res.json({
+      count_mutations: countMutation,
+      count_no_mutation: countNoMutation,
+      ratio: ratio
+    })
+  } catch (error) {
+    res.json({error: error.message})
+  }
 })
 
 app.post('/mutation', jsonParser, function(req, res) {
@@ -17,8 +31,10 @@ app.post('/mutation', jsonParser, function(req, res) {
   try {
     const mutation = hasMutation(dna)
     if (mutation) {
+      db.addMutation()
       res.status(200).end()
     } else {
+      db.addNoMutation()
       res.status(403).end()
     }
   } catch(err) {
