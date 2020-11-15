5cf6a63d114de787c5a2e0ed68ffc16afe14c574
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-11-15 11:48:23 -0600

    Create methods to get total of mutations and no mutation
    Both methods use a propise to handle the SQL query, which is
    asyncronous.

--- utils/database.js
+++ utils/database.js
@@ -25,6 +25,24 @@ class Database {
     })
   }
 
+  getCountMutations() {
+    return new Promise((resolve, reject) => {
+      this.conn.query('SELECT COUNT(*) AS count FROM mutations WHERE TYPE = "1"', function(err, results) {
+        if (err) reject(err.message)
+        if(results) resolve(results[0].count)
+      })
+    })
+  }
+
+  getCountNoMutations() {
+    return new Promise((resolve, reject) => {
+      this.conn.query('SELECT COUNT(*) AS count FROM mutations WHERE TYPE = "0"', function(err, results) {
+        if (err) reject(err.message)
+        if(results) resolve(results[0].count)
+      })
+    })
+  }
+
   startConnection() {
     this.conn.connect()
   }
