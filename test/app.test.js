cd90b19fe837457a9d410b21c66697b58b85a228
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-11-16 14:20:02 -0600

    Complete all required test

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,27 @@
 const hasMutation = require('dna-mutation');
 
-describe("Test has mutation: ", () => {
-  it("Horizontal Function: ", () => {
-    expect(hasMutation(["ATGCGA", "CAGTGC", "TTATCT", "AGACGG", "CCCCTA", "TCACTG"])).toBe(true)
+describe('Mutación en secuencia de ADN', () => {
+  test('Prueba de mutación horizontal', () => {
+    expect(hasMutation(['AAAAAA', 'CAGTGC', 'TTTTTT', 'AGACGG', 'CCCCCA', 'TGGGGG'])).toBe(true)
+  })
+  test('Prueba de mutación vertical', () => {
+    expect(hasMutation(['ATGCGA', 'ACGTGA', 'ATATTA', 'AGACGA', 'GCGTCC', 'TCACTG'])).toBe(true)
+  })
+  test('Prueba de mutación oblicua', () => {
+    expect(hasMutation(['AGGCGA', 'CAGTGC', 'TTAGTT', 'ATAAGG', 'GCTTCA', 'TCATTG'])).toBe(true)
+  })
+  test('Prueba de mutación oblicua invertida', () => {
+    expect(hasMutation(['ATGCGA', 'CAGGAC', 'TTGACT', 'AGACGG', 'GACTCA', 'TCGCTG'])).toBe(true)
+  })
+  test('Prueba de mutación mixta', () => {
+    expect(hasMutation(['AAAAGA', 'CAGGAC', 'TTGACT', 'TGTCGG', 'TACTCA', 'TCGCTG'])).toBe(true)
+  })
+  test('Prueba de no mutación', () => {
+    expect(hasMutation(['ATGCGA', 'CAGTGC', 'TTATTT', 'AGACGG', 'GCGTCA', 'TCACTG'])).toBe(false)
+  })
+  test('Prueba con caracteres erroneos', () => {
+    expect(() => {
+      hasMutation(['ATGCGA', 'CAZTGC', 'TTAHTT', 'AGAYGG', 'GCGTCA', 'TCACTG'])
+    }).toThrow()
   })
 })
